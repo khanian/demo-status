41698ae06e829f4c99858a3cabe5456156dd1e33
이강훈 <khanylee@gmail.com>
2023-04-07 19:21:53 +0900

    Initial commit

---
+++ src/main/java/com/example/demostatus/DemoStatusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demostatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStatusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demostatus/DemoStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demostatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
